4cd0dc69c076aca39e638d455380cb73834da716
ctdvu <dinhvucaotuan@gmail.com>
2023-07-16 10:29:58 +0700

    c1

---
+++ Hoc/C,C++/nhapxuat.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    printf ("nhap a b c d\n");
+    int a;
+    long long b;
+    float c;
+    double d;
+    scanf ("%d %lld %f %lf", &a, &b, &c, &d);
+    printf("cac so da nhap\n %d %lld %f %lf", a,b,c,d);
+    return 0;
+}
---
+++ Hoc/C,C++/test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+    printf("binh bo k co tuoi");
+    getch();
+    return 0;
+}
